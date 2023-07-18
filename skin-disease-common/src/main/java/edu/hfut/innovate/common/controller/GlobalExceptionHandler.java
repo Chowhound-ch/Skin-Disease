565740d8e3bffc730d5b1ea64bf9fa73c741f789
Chowhound <825352674@qq.com>
2023-07-19 00:27:30 +0800

    增加全局异常处理，统一异常响应格式

---
+++ skin-disease-common/src/main/java/edu/hfut/innovate/common/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package edu.hfut.innovate.common.controller;
+
+import edu.hfut.innovate.common.renren.R;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器,统一异常处理及其返回
+ *
+ * @author : Chowhound
+ * @since : 2023/7/19 - 0:22
+ */
+@Configuration
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e) {
+        e.printStackTrace();
+        return R.error(e.getMessage());
+    }
+}
