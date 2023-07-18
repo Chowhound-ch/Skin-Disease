9dd018b4679ebf86bd45de235bd865e91b7901b9
Chowhound <825352674@qq.com>
2023-07-19 00:00:25 +0800

    配置Mybatis-Plus分页插件

---
+++ skin-disease-common/src/main/java/edu/hfut/innovate/common/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package edu.hfut.innovate.common.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Chowhound
+ * @since : 2023/7/18 - 23:56
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 配置Mybatis-Plus分页插件
+     *
+     * @author : Chowhound
+     * @since : 2023/07/18 - 23:57
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 向MyBatis-Plus的过滤器链中添加分页拦截器，需要设置数据库类型（主要用于分页方言）
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
--- skin-disease-community-9001/src/main/java/edu/hfut/innovate/community/CommunityApplication.java
+++ skin-disease-community-9001/src/main/java/edu/hfut/innovate/community/CommunityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("edu.hfut.innovate.community.dao")
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "edu.hfut.innovate")
 public class CommunityApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class);
